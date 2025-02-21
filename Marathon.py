dff3323f447e4677f8d0bade4992d29249a9845e
edenmengste <edenmengste@aastustudent.edu.et>
2025-02-22 00:25:17 +0300

    updated

---
+++ Marathon.py
@@ -0,0 +1,11 @@
+def check():
+	counter = 0
+	a,b,c,d = map(int, input().split())
+	if b>a: 
+		counter += 1
+		
+
+
+t=int(input())
+for _ in range(t):
+	check()
---
+++ love story.py
@@ -0,0 +1,12 @@
+def check():
+	this= input()
+	difference = 0
+	if i in range(10):
+		if this[i] != "codeforces"[i]:
+			difference += 1
+	print("difference")		
+
+
+t=int(input())
+for _ in range():
+   check
